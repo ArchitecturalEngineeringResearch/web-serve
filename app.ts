4e721cd141a78a411fc98392b1354ca5a4dcace1
usernameisMan <vip939950559@qq.com>
2019-11-15 11:27:03 +0800

    feat: 加入 bodyParser

--- app.ts
+++ app.ts
@@ -1,10 +1,19 @@
 import * as Express from "express";
 import {user, messageList} from './controller'
+import Mongoose from 'mongoose'
+import * as bodyParser from 'body-parser'
 
 const app = Express()
 
+app.use(bodyParser.urlencoded({ extended: true }))
+
 app.use('/user',user)
 app.use('/messageList',messageList)
+
+app.use('*', (req, res) => {
+  res.status(404);
+  res.send('404');
+});
 
 app.listen(3000);
 console.log('listening to port 3000');
