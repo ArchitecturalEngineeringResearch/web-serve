8e2fb040c159254ea95113513a7089c3b3dfc968
usernameisMan <vip939950559@qq.com>
2019-11-19 20:56:26 +0800

    feat: 定义 createMassage 验证

--- controller/MessageList/validator.ts
+++ controller/MessageList/validator.ts
@@ -1,7 +1,62 @@
 import { CreateMassage } from './types'
+import { ParamSchema }  from 'express-validator' 
+import { number } from '../../util/regExps'
 
-export const createMassage: CreateMassage = {
-  
+export const createMassage: Record<keyof CreateMassage, ParamSchema> = {
+  title: {
+    exists:{
+      errorMessage: '必须是存在',
+    },
+    isString: {
+      errorMessage: '必须是 String',
+    },
+    isLength: {
+      options: {
+        max: 20,
+        min: 5
+      }
+    }
+  },
+  description: {
+    exists:{
+      errorMessage: '必须是存在',
+    },
+    isString: {
+      errorMessage: '必须是 String',
+    },
+    isLength: {
+      options: {
+        max: 150,
+        min: 5
+      }
+    }
+  },
+  phoneNumber: {
+    exists:{
+      errorMessage: '必须是存在',
+    },
+    isInt: {
+      errorMessage: '必须是 Int',
+    },
+    custom: {
+      options: (value) => number.china_phone.test(value),
+      errorMessage: '手机号码无法通过验证',
+    }
+  }, 
+  endDate: {
+    exists:{
+      errorMessage: '必须是存在',
+    },
+    isString: {
+      errorMessage: '必须是 String',
+    },
+    isLength: {
+      options: {
+        max: 20,
+      }
+    }
+  },
+  createDate: {},
 }
 
 const validator = {
