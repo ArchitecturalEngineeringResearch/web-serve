d2e189505b84c3c84ca97dcf567fa1c86c242580
usernameisMan <vip939950559@qq.com>
2019-11-21 14:18:15 +0800

    feat: add mongodb

--- app.ts
+++ app.ts
@@ -1,11 +1,21 @@
 import * as Express from "express";
 import * as bodyParser from 'body-parser'
+import { MongoClient } from 'mongodb'
 
 import {user, messageList} from './controller'
 import { NOT_FOUND_404 } from "./util/httpStatus";
 import { MongoClientURL } from "./config";
 
 const app = Express()
+
+const client = new MongoClient(MongoClientURL, { useNewUrlParser: true });
+client.connect(err => {
+  console.log('mongodb ------ 连接成功')
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
 
 app.use(bodyParser.urlencoded({ extended: true }))
 
---
+++ config.ts
@@ -0,0 +1,1 @@
+export const MongoClientURL = 'mongodb+srv://lenlee:qazwsx123@cluster0-gp2nn.azure.mongodb.net/test?retryWrites=true&w=majority'
