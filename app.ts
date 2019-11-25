47a8047dc131603e3ee0336745b5a751998fe652
usernameisMan <vip939950559@qq.com>
2019-11-25 09:38:46 +0800

    refactor: node mongo init

--- app.ts
+++ app.ts
@@ -1,39 +1,52 @@
 import * as Express from "express";
 import * as bodyParser from 'body-parser'
-import { connect, connection } from 'mongoose'
+import { connect } from 'mongoose'
 
 import {user, messageList} from './controller'
 import { NOT_FOUND_404 } from "./util/httpStatus";
-import { MongoClientURL } from "./config";
+import { MongoClientURL, MongoClientDBName, portNumber } from "./config";
 
 const app = Express()
 
-connect(
-  MongoClientURL,
-  { 
-    dbName: 'zc',
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+function connectMongodb(): Promise<typeof import("mongoose")> {
+  return connect(
+    MongoClientURL,
+    { 
+      dbName: MongoClientDBName,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  
+}
+
+async function startAPP() {
+  await connectMongodb().then(() => {
+    console.log(`
+      Mongodb Access Successful:
+        DBname: ${MongoClientDBName}
+        Address: ${MongoClientURL}`)
+      runServer()
+  }).catch((error)=> {
+    console.error(error)
   })
+}
 
-connection.on('open' ,()=> {
-  console.log('open')
-})
+function runServer () {
+  app.use(bodyParser.urlencoded({ extended: true }))
 
-connection.on('error' ,()=> {
-  console.log('error')
-})
+  app.use('/user',user)
+  app.use('/messageList',messageList)
 
+  app.use('*', (req, res) => {
+    res.status(NOT_FOUND_404);
+    res.send(NOT_FOUND_404);
+  });
 
-app.use(bodyParser.urlencoded({ extended: true }))
+  app.listen(portNumber);
+  console.log(`
+    Server listening to port ${portNumber}
+  `);
+}
 
-app.use('/user',user)
-app.use('/messageList',messageList)
-
-app.use('*', (req, res) => {
-  res.status(NOT_FOUND_404);
-  res.send(NOT_FOUND_404);
-});
-
-app.listen(3000);
-console.log('listening to port 3000');
+startAPP()
