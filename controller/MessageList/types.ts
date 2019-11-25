55f8048409f2313385c0308c77269e43b813f7b0
usernameisMan <vip939950559@qq.com>
2019-11-25 16:46:01 +0800

    refactor: 重新设计类型文件位置

--- controller/MessageList/types.ts
+++
@@ -1,7 +0,0 @@
-export interface CreateMassage {
-  title: string;
-  description: string; 
-  phoneNumber: number; 
-  createDate: string | number; 
-  endDate: string | number; 
-}
---
+++ types/IMessageList.ts
@@ -0,0 +1,11 @@
+export interface ICreateMassage {
+  title: string;
+  description: string; 
+  phoneNumber: number; 
+  createDate: string | number; 
+  endDate: string | number;
+  province: string;
+  city: string;
+  longitude: string;
+  latitude: string;
+}
