45244745f128382678bfe98a53b9e9eba7831789
usernameisMan <vip939950559@qq.com>
2019-11-25 16:46:46 +0800

    feat: 定义MessageSchema

---
+++ models/MessageList/schema.ts
@@ -0,0 +1,16 @@
+import { Schema } from 'mongoose'
+import { IMessageList } from '../../types'
+
+type IMessageSchema = Record<keyof IMessageList.ICreateMassage, any>
+
+export const MessageSchema: Schema<IMessageSchema> = new Schema({
+  title: String,
+  description: String, 
+  phoneNumber: Number,
+  createDate: [String,Number],
+  endDate: [String,Number],
+  province: String,
+  city: String,
+  longitude: String,
+  latitude: String,
+})
