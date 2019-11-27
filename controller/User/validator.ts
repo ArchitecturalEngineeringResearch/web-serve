4a1c2dadf2efc26d2c0b494d014714b23f9f1968
usernameisMan <vip939950559@qq.com>
2019-11-28 00:21:44 +0800

    feat: 加入 user 请求验证

--- controller/User/validator.ts
+++ controller/User/validator.ts
@@ -1,7 +1,43 @@
-import { check }  from 'express-validator'
+import { ParamSchema }  from 'express-validator'
+import { IwechatCredentials } from '../../types/IUser'
+
+export const loginValidator : Record<keyof IwechatCredentials, ParamSchema> = {
+  appid: {
+    exists:{
+      errorMessage: '必须是存在',
+    },
+    isString: {
+      errorMessage: '必须是 String',
+    },
+  },
+  secret: {
+    exists:{
+      errorMessage: '必须是存在',
+    },
+    isString: {
+      errorMessage: '必须是 String',
+    },
+  },
+  jsCode: {
+    exists:{
+      errorMessage: '必须是存在',
+    },
+    isString: {
+      errorMessage: '必须是 String',
+    },
+  },
+  authorization_code: {
+    exists:{
+      errorMessage: '必须是存在',
+    },
+    isString: {
+      errorMessage: '必须是 String',
+    },
+  },
+}
 
 const validator = {
-
+  loginValidator
 }
 
 export default validator
