caf1597ab82e97b4f20ac3e8650884efd638a43d
usernameisMan <vip939950559@qq.com>
2019-11-28 00:20:21 +0800

    feat: 微信验证的接口配置

--- config.ts
+++ config.ts
@@ -1,3 +1,4 @@
 export const MongoClientURL = 'mongodb+srv://lenlee:qazwsx123@cluster0-gp2nn.azure.mongodb.net/test?retryWrites=true&w=majority'
 export const MongoClientDBName = 'zc'
 export const portNumber = 3000
+export const wechatCredentialsUrl ='https://api.weixin.qq.com/sns/jscode2session'
