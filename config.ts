e9cb93739df4d98d80a7771e64bb186c0dfbfd57
usernameisMan <vip939950559@qq.com>
2020-04-08 16:58:00 +0800

    feat: 改变ssl 证书

--- config.ts
+++ config.ts
@@ -2,3 +2,5 @@ export const MongoClientURL = 'mongodb+srv://lenlee:qazwsx123@cluster0-gp2nn.azu
 export const MongoClientDBName = 'zc'
 export const portNumber = 3000
 export const wechatCredentialsUrl ='https://api.weixin.qq.com/sns/jscode2session'
+
+// _Qazwsx123
